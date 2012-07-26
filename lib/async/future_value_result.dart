826388a5e218ad85d5bbdc657393029f6cbae11e
Kevin Moore <kevin@thinkpixellab.com>
2012-07-26 09:29:44 -0700

    Looking at a serialization model for FutureValueResult

--- lib/async/future_value_result.dart
+++ lib/async/future_value_result.dart
@@ -1,4 +1,7 @@
 class FutureValueResult<TOutput> {
+  static final String _valueKey = 'value';
+  static final String _exceptionKey = 'exception';
+
   final TOutput value;
   final exception;
 
@@ -6,7 +9,37 @@ class FutureValueResult<TOutput> {
   : exception = null;
 
   FutureValueResult.exception(this.exception)
-  : value = null;
+  : value = null {
+    requireArgumentNotNull(exception, 'exception');
+  }
+
+  factory FutureValueResult.fromMap(Map value) {
+    requireArgumentNotNull(value, 'value');
+    requireArgument(isMyMap(value), 'value');
+
+    final ex = value[_exceptionKey];
+    if(ex != null) {
+      return new FutureValueResult.exception(ex);
+    } else {
+      return new FutureValueResult(value[_valueKey]);
+    }
+  }
 
   bool get isException() => exception != null;
+
+  Map toMap() {
+    // would love to use consts here, but the analyzer doesn't like it
+    // http://code.google.com/p/dart/issues/detail?id=4207
+    return { 'value' : value, 'exception' : exception };
+  }
+
+  static bool isMyMap(Map value) {
+    return value != null && value.length == 2 &&
+        value.containsKey(_valueKey) && value.containsKey(_exceptionKey);
+  }
+
+  bool operator ==(FutureValueResult other) {
+    return other !== null &&
+        other.value == value && other.exception == exception;
+  }
 }
